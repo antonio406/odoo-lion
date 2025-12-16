7dc251dbce0f53bb758126b88030e02b03fcef67
Antonio Dimas <21302127@utfv.edu.mx>
2025-12-15 18:04:05 -0600

    Add custom theme_lionsceller module

---
+++ custom_addons/theme_lionsceller/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Theme LionSceller (Enterprise Look)',
+    'version': '1.0',
+    'category': 'Theme/Backend',
+    'summary': 'Enterprise-like menu for Community',
+    'depends': ['web'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'theme_lionsceller/static/src/xml/navbar.xml',
+            'theme_lionsceller/static/src/scss/navbar.scss',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+}
